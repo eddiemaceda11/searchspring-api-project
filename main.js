2cfeb8f90e0140bb6377802b5db5aa777ee9bb29
Eddie Maceda <eddiemaceda11@gmail.com>
2023-03-20 12:11:40 -0400

    Created function that will render results on enter key/search icon click, or when user clicks on nav img item

--- main.js
+++ main.js
@@ -19,3 +19,81 @@ const imgLink2 = document.getElementById("2");
 const imgLink3 = document.getElementById("3");
 const imgLink4 = document.getElementById("4");
 const imgLink5 = document.getElementById("5");
+
+// Renders the results to the page after the user searches
+// or clicks on a valid search link
+async function renderResults(value) {
+  // check to see if search input is a page number
+  currentPage = search.value === Number ? search.value : 1;
+
+  // Display results, and search result text
+  results.classList.remove("hidden");
+  resultsMessage.textContent = value;
+
+  // Hide prev button if search result is the 1st page
+  if (currentPage === 1) {
+    prevPageBtns.forEach((btn) => {
+      btn.style.display = "none";
+    });
+    nextPageBtns.forEach((btn) => {
+      btn.style.display = "block";
+    });
+  }
+
+  let html = "";
+
+  // Make a call to the Searchspring API
+  const response = await fetch(`http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=${value}&resultsFormat=native&page=${currentPage}`);
+  data = await response.json();
+  data.results.forEach((result) => {
+    // CONDITIONALs
+    // If product has msrp and its greater than price,
+    // add proper style to msrp and price
+
+    // If product image returns empty/error, display a default image
+    if (result.msrp && result.msrp > result.price) {
+      html += `
+        <div class="card">
+          <img class="card-img" src=${result.thumbnailImageUrl} onerror="this.onerror=null;this.src='img/404_page_cover.jpg';" />
+          <h3 class="item-name">${result.name}</h3>
+          <div class="card-pricing">
+            <p style="text-decoration: line-through;">$${result.msrp}.00</p>
+            <p style="color: red; font-weight: 700;">$${result.price}.00</p>
+            <i class="fa-solid fa-cart-shopping" id="add-to-cart"></i>
+          </div>
+        </div>
+      `;
+    } else {
+      // If price is greater than or equal to msrp,
+      // do not display msrp
+      html += `
+        <div class="card">
+          <img class="card-img" src=${result.thumbnailImageUrl} onerror="this.onerror=null;this.src='img/404_page_cover.jpg';" />
+          <h3 class="item-name">${result.name}</h3>
+          <div class="card-pricing">
+          <p style="font-weight: 700;">$${result.price}.00</p>
+            <i class="fa-solid fa-cart-shopping" id="add-to-cart"></i>
+          </div>
+        </div>
+      `;
+    }
+  });
+
+  // Clear previous grid container results
+  gridDisplay.innerHTML = "";
+  // Add new result cards to grid container
+  gridDisplay.innerHTML += html;
+  // If total pages of search results = 1, hide the prev/next button
+  if (data.pagination.totalPages === 1) {
+    hideButtons();
+  }
+
+  // If search result is invalid, display invalid message
+  if (gridDisplay.innerHTML === "") {
+    resultsMessage.textContent = "No Results Found";
+    hideButtons();
+  }
+  // Scroll to results section
+  scroll();
+  return data;
+}
